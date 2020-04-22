2f7e3b5a755e6d7501645d5061ab38b777304491
euphobyte <56632888+euphobyte@users.noreply.github.com>
2020-04-22 12:39:13 +0200

    Update hello.py

--- python3/hello.py
+++ python3/hello.py
@@ -1,10 +1,4 @@
 # Run: python hello.py
-# Result:
-# Hello World! 0
-#
-# Hello World! 1
-# 
-# Hello World! 2
 # To have only a single newline remove '\n' from print statement
 # Variable at the end adds an automatic newline
 # To print everything on one line add a ',' after the variable at the end of the print statement
